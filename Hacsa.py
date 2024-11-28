e0903fc7e461ce0fb09e57c70339ba3b73f350cc
blessphilips <blessphilips636@gmail.com>
2024-11-28 15:13:42 +0000

    Hacsa-First-Commit

---
+++ Hacsa.py
@@ -0,0 +1,7 @@
+name= input("please type your name:")
+age= input("please enter your age:")
+school= input("please enter your school:")
+address= input("please enter your address:")
+password= input("input password:")
+confirm_password= input("please confrim password:")
+
